4dce8d7bc87349fae55c077243afaea4ac9e1b26
quocphu <nguyenduyquocphu1998@gmail.com>
2020-06-23 10:00:48 +0700

    Updates

---
+++ precache-manifest.1f0779576e8483ec504865a756705e22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2917896e00c72765e1d0d7e734602e7f",
+    "url": "/ffd-client/index.html"
+  },
+  {
+    "revision": "0eadb196c751ecd884fc",
+    "url": "/ffd-client/static/css/2.28278f22.chunk.css"
+  },
+  {
+    "revision": "5983957e48f885cfbe8a",
+    "url": "/ffd-client/static/css/main.851ddd16.chunk.css"
+  },
+  {
+    "revision": "0eadb196c751ecd884fc",
+    "url": "/ffd-client/static/js/2.2d0f8583.chunk.js"
+  },
+  {
+    "revision": "0f1f8b8b339bb848aa87221a1db928a8",
+    "url": "/ffd-client/static/js/2.2d0f8583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5983957e48f885cfbe8a",
+    "url": "/ffd-client/static/js/main.4561885b.chunk.js"
+  },
+  {
+    "revision": "9653d760ca067f339108",
+    "url": "/ffd-client/static/js/runtime-main.5a5749c3.js"
+  }
+]);
